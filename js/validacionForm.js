5ffd01900b22146d7e8120cdf7e44e05ce98144e
MGIC12 <mager.infante@gmail.com>
2024-06-07 12:37:33 -0300

    changes

--- js/validacionForm.js
+++ js/validacionForm.js
@@ -1,6 +1,13 @@
 const nombre = document.getElementById("name");
+const inputNombre = document.querySelector("input[name='name']");
+const apellido = document.getElementById("last-name");
+const inputLastName = document.querySelector("input[name='last-name']");
 const email = document.getElementById("email");
+const inputEmail = document.querySelector("input[name='email']");
+const country = document.getElementById("country");
+const inputCountry = document.querySelector("select[name='country']");
 const message = document.getElementById("message");
+const inputMessage = document.querySelector("textarea[name='message']");
 const form = document.getElementById("form");
 
 form.addEventListener("submit", (e) => {
@@ -8,7 +15,12 @@ form.addEventListener("submit", (e) => {
   let regexEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,4})+$/;
   let regexName = /^[A-Za-zÑñÁáÉéÍíÓóÚúÜü\s]+$/;
   let entrar = false;
-  if (nombre.value.length < 6) {
+  //Validación nombre
+  if (
+    nombre.value.length < 3 ||
+    nombre.value.length > 20 ||
+    inputNombre.value.trim() === ""
+  ) {
     nombre.style.border = "2px solid red";
     alert("El nombre es demasiado corto");
     entrar = true;
@@ -16,23 +28,74 @@ form.addEventListener("submit", (e) => {
     nombre.style.border = "2px solid red";
     alert("Introduce un nombre correcto");
     entrar = true;
+  } else if (nombre.value !== nombre.value.trim()) {
+    nombre.style.border = "2px solid red";
+    alert("El nombre no debe contener espacios al inicio o final");
+    entrar = true;
   } else {
     nombre.style.border = "2px solid green";
   }
-  if (!regexEmail.test(email.value)) {
+
+  //Validación apellido
+  if (
+    apellido.value.length < 4 ||
+    apellido.value.length > 20 ||
+    inputLastName.value.trim() === ""
+  ) {
+    apellido.style.border = "2px solid red";
+    alert("El apellido es demasiado corto");
+    entrar = true;
+  } else if (!regexName.test(apellido.value)) {
+    apellido.style.border = "2px solid red";
+    alert("Introduce un apellido correcto");
+    entrar = true;
+  } else if (apellido.value !== apellido.value.trim()) {
+    apellido.style.border = "2px solid red";
+    alert("El apellido no debe contener espacios al inicio o final");
+    entrar = true;
+  } else {
+    apellido.style.border = "2px solid green";
+  }
+
+  //Validación país
+  if (country.value == "") {
+    country.style.border = "2px solid red";
+    alert("Por favor, seleccione un pais");
+    entrar = true;
+  } else {
+    country.style.border = "2px solid green";
+  }
+
+  //Validación email
+  if (!regexEmail.test(email.value) || inputEmail.value.trim() === "") {
     email.style.border = "2px solid red";
     alert("Introduce un email correcto");
+    entrar = true;
+  } else if (email.value !== email.value.trim()) {
+    email.style.border = "2px solid red";
+    alert("El email no debe contener espacios al inicio o final");
     entrar = true;
   } else {
     email.style.border = "2px solid green";
   }
-  if (message.value.length < 10) {
+
+  //Validación mensaje
+  if (message.value.length < 10 || inputMessage.value.trim() === "") {
     message.style.border = "2px solid red";
     alert("El mensaje es demasiado corto");
+    entrar = true;
+  } else if (message.value !== message.value.trim()) {
+    message.style.border = "2px solid red";
+    alert("El mensaje no debe contener espacios al inicio o final");
+    entrar = true;
+  } else if (message.value.length > 500) {
+    message.style.border = "2px solid red";
+    alert("El mensaje es demasiado extenso");
     entrar = true;
   } else {
     message.style.border = "2px solid green";
   }
+
   if (!entrar) {
     alert("Formulario enviado, gracias por contactarte! :)");
     form.reset();
