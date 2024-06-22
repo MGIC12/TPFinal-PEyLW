558dca3e0e2109f637a6a9b7274618960fd69256
MGIC12 <mager.infante@gmail.com>
2024-06-21 23:17:36 -0300

    change

---
+++ js/langSelectorIndex.js
@@ -0,0 +1,28 @@
+const flagsElement = document.getElementById("flags");
+
+const textsToChange = document.querySelectorAll("[data-section]");
+
+const changeLanguage = async (language) => {
+  const requestJson = await fetch(`js/languages/${language}.json`);
+  const texts = await requestJson.json();
+
+  // Guardar el idioma seleccionado en el almacenamiento local
+  localStorage.setItem("selectedLanguage", language);
+
+  for (const textToChange of textsToChange) {
+    const section = textToChange.dataset.section;
+    const value = textToChange.dataset.value;
+
+    textToChange.innerHTML = texts[section][value];
+  }
+};
+
+// Verificar si hay un idioma seleccionado previamente en el almacenamiento local
+const selectedLanguage = localStorage.getItem("selectedLanguage");
+if (selectedLanguage) {
+  changeLanguage(selectedLanguage);
+}
+
+flagsElement.addEventListener("click", (e) => {
+  changeLanguage(e.target.parentElement.dataset.language);
+});
